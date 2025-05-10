2a133112626f9b231bd748a9b3d24f92cad9be56
suphaashyy <131824718+metalis3z@users.noreply.github.com>
2025-05-10 09:48:07 +0700

    Create pulser.lua

---
+++ pulser.lua
@@ -0,0 +1,352 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local Nighmare = Instance.new("ScreenGui")
+local Frame1 = Instance.new("Frame")
+local HiDaphnehowwasyourday = Instance.new("TextLabel")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+local youareveryspecialtous = Instance.new("TextLabel")
+local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_2 = Instance.new("UITextSizeConstraint")
+local catcatandLunarwilllookafteryoudontbeafraidDaphne = Instance.new("TextLabel")
+local UIAspectRatioConstraint_3 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_3 = Instance.new("UITextSizeConstraint")
+local itcanbebadifyoudontcheckyourscreenoften = Instance.new("TextLabel")
+local UIAspectRatioConstraint_4 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_4 = Instance.new("UITextSizeConstraint")
+local howareyoufeelingtoday = Instance.new("TextLabel")
+local UIAspectRatioConstraint_5 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_5 = Instance.new("UITextSizeConstraint")
+local dontforgetaboutyourfarming = Instance.new("TextLabel")
+local UIAspectRatioConstraint_6 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_6 = Instance.new("UITextSizeConstraint")
+local dontworrydaphnetheywontbeabletocatchyou = Instance.new("TextLabel")
+local UIAspectRatioConstraint_7 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_7 = Instance.new("UITextSizeConstraint")
+local maybewecanbetogetherinanotherdimension = Instance.new("TextLabel")
+local UIAspectRatioConstraint_8 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_8 = Instance.new("UITextSizeConstraint")
+local _666 = Instance.new("TextLabel")
+local UIAspectRatioConstraint_9 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_9 = Instance.new("UITextSizeConstraint")
+local _667 = Instance.new("TextLabel")
+local UIAspectRatioConstraint_10 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_10 = Instance.new("UITextSizeConstraint")
+local _112 = Instance.new("TextLabel")
+local UIAspectRatioConstraint_11 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_11 = Instance.new("UITextSizeConstraint")
+local _668 = Instance.new("TextLabel")
+local UIAspectRatioConstraint_12 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_12 = Instance.new("UITextSizeConstraint")
+local _14 = Instance.new("TextLabel")
+local UIAspectRatioConstraint_13 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_13 = Instance.new("UITextSizeConstraint")
+local _50 = Instance.new("TextLabel")
+local UIAspectRatioConstraint_14 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_14 = Instance.new("UITextSizeConstraint")
+local UIAspectRatioConstraint_15 = Instance.new("UIAspectRatioConstraint")
+local UIAspectRatioConstraint_16 = Instance.new("UIAspectRatioConstraint")
+
+--Properties:
+
+Nighmare.Name = "Nighmare"
+Nighmare.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+Frame1.Name = "Frame 1"
+Frame1.Parent = Nighmare
+Frame1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame1.BorderSizePixel = 0
+Frame1.Size = UDim2.new(1, 0, 1, 0)
+
+HiDaphnehowwasyourday.Name = "Hi Daphne, how was your day?"
+HiDaphnehowwasyourday.Parent = Frame1
+HiDaphnehowwasyourday.BackgroundTransparency = 1.000
+HiDaphnehowwasyourday.BorderSizePixel = 0
+HiDaphnehowwasyourday.Position = UDim2.new(0.0401250646, 0, 0.128822982, 0)
+HiDaphnehowwasyourday.Size = UDim2.new(0.22720167, 0, 0.0379981473, 0)
+HiDaphnehowwasyourday.Font = Enum.Font.Unknown
+HiDaphnehowwasyourday.Text = "Hi Daphne, how was your day?"
+HiDaphnehowwasyourday.TextColor3 = Color3.fromRGB(255, 255, 255)
+HiDaphnehowwasyourday.TextScaled = true
+HiDaphnehowwasyourday.TextSize = 40.000
+HiDaphnehowwasyourday.TextWrapped = true
+HiDaphnehowwasyourday.TextXAlignment = Enum.TextXAlignment.Left
+HiDaphnehowwasyourday.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint.Parent = HiDaphnehowwasyourday
+UIAspectRatioConstraint.AspectRatio = 10.634
+
+UITextSizeConstraint.Parent = HiDaphnehowwasyourday
+UITextSizeConstraint.MaxTextSize = 40
+
+youareveryspecialtous.Name = "you are very special to us"
+youareveryspecialtous.Parent = Frame1
+youareveryspecialtous.BackgroundTransparency = 1.000
+youareveryspecialtous.BorderSizePixel = 0
+youareveryspecialtous.Position = UDim2.new(0.0489838459, 0, 0.252085268, 0)
+youareveryspecialtous.Size = UDim2.new(0.186555505, 0, 0.0379981473, 0)
+youareveryspecialtous.Font = Enum.Font.Unknown
+youareveryspecialtous.Text = "you are very special to us"
+youareveryspecialtous.TextColor3 = Color3.fromRGB(255, 255, 255)
+youareveryspecialtous.TextScaled = true
+youareveryspecialtous.TextSize = 40.000
+youareveryspecialtous.TextWrapped = true
+youareveryspecialtous.TextXAlignment = Enum.TextXAlignment.Left
+youareveryspecialtous.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_2.Parent = youareveryspecialtous
+UIAspectRatioConstraint_2.AspectRatio = 8.732
+
+UITextSizeConstraint_2.Parent = youareveryspecialtous
+UITextSizeConstraint_2.MaxTextSize = 40
+
+catcatandLunarwilllookafteryoudontbeafraidDaphne.Name = "catcat and Lunar will look after you, don't be afraid Daphne"
+catcatandLunarwilllookafteryoudontbeafraidDaphne.Parent = Frame1
+catcatandLunarwilllookafteryoudontbeafraidDaphne.BackgroundTransparency = 1.000
+catcatandLunarwilllookafteryoudontbeafraidDaphne.BorderSizePixel = 0
+catcatandLunarwilllookafteryoudontbeafraidDaphne.Position = UDim2.new(0.170401245, 0, 0.0648748875, 0)
+catcatandLunarwilllookafteryoudontbeafraidDaphne.Size = UDim2.new(0.426784784, 0, 0.0379981473, 0)
+catcatandLunarwilllookafteryoudontbeafraidDaphne.Font = Enum.Font.Unknown
+catcatandLunarwilllookafteryoudontbeafraidDaphne.Text = "catcat and Lunar will look after you, don't be afraid Daphne"
+catcatandLunarwilllookafteryoudontbeafraidDaphne.TextColor3 = Color3.fromRGB(255, 255, 255)
+catcatandLunarwilllookafteryoudontbeafraidDaphne.TextScaled = true
+catcatandLunarwilllookafteryoudontbeafraidDaphne.TextSize = 40.000
+catcatandLunarwilllookafteryoudontbeafraidDaphne.TextWrapped = true
+catcatandLunarwilllookafteryoudontbeafraidDaphne.TextXAlignment = Enum.TextXAlignment.Left
+catcatandLunarwilllookafteryoudontbeafraidDaphne.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_3.Parent = catcatandLunarwilllookafteryoudontbeafraidDaphne
+UIAspectRatioConstraint_3.AspectRatio = 19.976
+
+UITextSizeConstraint_3.Parent = catcatandLunarwilllookafteryoudontbeafraidDaphne
+UITextSizeConstraint_3.MaxTextSize = 40
+
+itcanbebadifyoudontcheckyourscreenoften.Name = "it can be bad if you don't check your screen often"
+itcanbebadifyoudontcheckyourscreenoften.Parent = Frame1
+itcanbebadifyoudontcheckyourscreenoften.BackgroundTransparency = 1.000
+itcanbebadifyoudontcheckyourscreenoften.BorderSizePixel = 0
+itcanbebadifyoudontcheckyourscreenoften.Position = UDim2.new(0.146430433, 0, 0.527340114, 0)
+itcanbebadifyoudontcheckyourscreenoften.Size = UDim2.new(0.335591465, 0, 0.0379981473, 0)
+itcanbebadifyoudontcheckyourscreenoften.Font = Enum.Font.Unknown
+itcanbebadifyoudontcheckyourscreenoften.Text = "it can be bad if you don't check your screen often"
+itcanbebadifyoudontcheckyourscreenoften.TextColor3 = Color3.fromRGB(255, 255, 255)
+itcanbebadifyoudontcheckyourscreenoften.TextScaled = true
+itcanbebadifyoudontcheckyourscreenoften.TextSize = 40.000
+itcanbebadifyoudontcheckyourscreenoften.TextWrapped = true
+itcanbebadifyoudontcheckyourscreenoften.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_4.Parent = itcanbebadifyoudontcheckyourscreenoften
+UIAspectRatioConstraint_4.AspectRatio = 15.707
+
+UITextSizeConstraint_4.Parent = itcanbebadifyoudontcheckyourscreenoften
+UITextSizeConstraint_4.MaxTextSize = 40
+
+howareyoufeelingtoday.Name = "how are you feeling today?"
+howareyoufeelingtoday.Parent = Frame1
+howareyoufeelingtoday.BackgroundTransparency = 1.000
+howareyoufeelingtoday.BorderSizePixel = 0
+howareyoufeelingtoday.Position = UDim2.new(0.458051056, 0, 0.282669127, 0)
+howareyoufeelingtoday.Size = UDim2.new(0.196977586, 0, 0.0379981473, 0)
+howareyoufeelingtoday.Font = Enum.Font.Unknown
+howareyoufeelingtoday.Text = "how are you feeling today?"
+howareyoufeelingtoday.TextColor3 = Color3.fromRGB(255, 255, 255)
+howareyoufeelingtoday.TextScaled = true
+howareyoufeelingtoday.TextSize = 40.000
+howareyoufeelingtoday.TextWrapped = true
+howareyoufeelingtoday.TextXAlignment = Enum.TextXAlignment.Left
+howareyoufeelingtoday.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_5.Parent = howareyoufeelingtoday
+UIAspectRatioConstraint_5.AspectRatio = 9.220
+
+UITextSizeConstraint_5.Parent = howareyoufeelingtoday
+UITextSizeConstraint_5.MaxTextSize = 40
+
+dontforgetaboutyourfarming.Name = "don’t forget about your farming"
+dontforgetaboutyourfarming.Parent = Frame1
+dontforgetaboutyourfarming.BackgroundTransparency = 1.000
+dontforgetaboutyourfarming.BorderSizePixel = 0
+dontforgetaboutyourfarming.Position = UDim2.new(0.0125065139, 0, 0.376274318, 0)
+dontforgetaboutyourfarming.Size = UDim2.new(0.259510159, 0, 0.0379981473, 0)
+dontforgetaboutyourfarming.Font = Enum.Font.Unknown
+dontforgetaboutyourfarming.Text = "don’t forget about your farming"
+dontforgetaboutyourfarming.TextColor3 = Color3.fromRGB(255, 255, 255)
+dontforgetaboutyourfarming.TextScaled = true
+dontforgetaboutyourfarming.TextSize = 40.000
+dontforgetaboutyourfarming.TextWrapped = true
+dontforgetaboutyourfarming.TextXAlignment = Enum.TextXAlignment.Left
+dontforgetaboutyourfarming.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_6.Parent = dontforgetaboutyourfarming
+UIAspectRatioConstraint_6.AspectRatio = 12.146
+
+UITextSizeConstraint_6.Parent = dontforgetaboutyourfarming
+UITextSizeConstraint_6.MaxTextSize = 40
+
+dontworrydaphnetheywontbeabletocatchyou.Name = "don't worry daphne, they won't be able to catch you"
+dontworrydaphnetheywontbeabletocatchyou.Parent = Frame1
+dontworrydaphnetheywontbeabletocatchyou.BackgroundTransparency = 1.000
+dontworrydaphnetheywontbeabletocatchyou.BorderSizePixel = 0
+dontworrydaphnetheywontbeabletocatchyou.Position = UDim2.new(0.279312134, 0, 0.184430033, 0)
+dontworrydaphnetheywontbeabletocatchyou.Size = UDim2.new(0.379885346, 0, 0.0379981473, 0)
+dontworrydaphnetheywontbeabletocatchyou.Font = Enum.Font.Unknown
+dontworrydaphnetheywontbeabletocatchyou.Text = "don't worry daphne, they won't be able to catch you"
+dontworrydaphnetheywontbeabletocatchyou.TextColor3 = Color3.fromRGB(255, 255, 255)
+dontworrydaphnetheywontbeabletocatchyou.TextScaled = true
+dontworrydaphnetheywontbeabletocatchyou.TextSize = 40.000
+dontworrydaphnetheywontbeabletocatchyou.TextWrapped = true
+dontworrydaphnetheywontbeabletocatchyou.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_7.Parent = dontworrydaphnetheywontbeabletocatchyou
+UIAspectRatioConstraint_7.AspectRatio = 17.780
+
+UITextSizeConstraint_7.Parent = dontworrydaphnetheywontbeabletocatchyou
+UITextSizeConstraint_7.MaxTextSize = 40
+
+maybewecanbetogetherinanotherdimension.Name = "maybe we can be together in another dimension?"
+maybewecanbetogetherinanotherdimension.Parent = Frame1
+maybewecanbetogetherinanotherdimension.BackgroundTransparency = 1.000
+maybewecanbetogetherinanotherdimension.BorderSizePixel = 0
+maybewecanbetogetherinanotherdimension.Position = UDim2.new(0.293381959, 0, 0.445783138, 0)
+maybewecanbetogetherinanotherdimension.Size = UDim2.new(0.379885346, 0, 0.0472659878, 0)
+maybewecanbetogetherinanotherdimension.Font = Enum.Font.Unknown
+maybewecanbetogetherinanotherdimension.Text = "maybe we can be together in another dimension?"
+maybewecanbetogetherinanotherdimension.TextColor3 = Color3.fromRGB(255, 255, 255)
+maybewecanbetogetherinanotherdimension.TextScaled = true
+maybewecanbetogetherinanotherdimension.TextSize = 40.000
+maybewecanbetogetherinanotherdimension.TextWrapped = true
+maybewecanbetogetherinanotherdimension.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_8.Parent = maybewecanbetogetherinanotherdimension
+UIAspectRatioConstraint_8.AspectRatio = 14.294
+
+UITextSizeConstraint_8.Parent = maybewecanbetogetherinanotherdimension
+UITextSizeConstraint_8.MaxTextSize = 40
+
+_666.Name = "666"
+_666.Parent = Frame1
+_666.BackgroundTransparency = 1.000
+_666.BorderSizePixel = 0
+_666.Position = UDim2.new(0.258989066, 0, 0.340129763, 0)
+_666.Size = UDim2.new(0.0489838459, 0, 0.0463392027, 0)
+_666.Font = Enum.Font.Unknown
+_666.Text = "666\\n"
+_666.TextColor3 = Color3.fromRGB(255, 255, 255)
+_666.TextScaled = true
+_666.TextSize = 40.000
+_666.TextWrapped = true
+_666.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_9.Parent = _666
+UIAspectRatioConstraint_9.AspectRatio = 1.880
+
+UITextSizeConstraint_9.Parent = _666
+UITextSizeConstraint_9.MaxTextSize = 40
+
+_667.Name = "667"
+_667.Parent = Frame1
+_667.BackgroundTransparency = 1.000
+_667.BorderSizePixel = 0
+_667.Position = UDim2.new(0.391349673, 0, 0.240037069, 0)
+_667.Size = UDim2.new(0.0343929119, 0, 0.0426320657, 0)
+_667.Font = Enum.Font.Unknown
+_667.Text = "667\\n"
+_667.TextColor3 = Color3.fromRGB(255, 255, 255)
+_667.TextScaled = true
+_667.TextSize = 40.000
+_667.TextWrapped = true
+_667.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_10.Parent = _667
+UIAspectRatioConstraint_10.AspectRatio = 1.435
+
+UITextSizeConstraint_10.Parent = _667
+UITextSizeConstraint_10.MaxTextSize = 40
+
+_112.Name = "112"
+_112.Parent = Frame1
+_112.BackgroundTransparency = 1.000
+_112.BorderSizePixel = 0
+_112.Position = UDim2.new(0.257425755, 0, 0.252085268, 0)
+_112.Size = UDim2.new(0.0588848367, 0, 0.0379981473, 0)
+_112.Font = Enum.Font.Unknown
+_112.Text = "112"
+_112.TextColor3 = Color3.fromRGB(255, 255, 255)
+_112.TextScaled = true
+_112.TextSize = 40.000
+_112.TextWrapped = true
+_112.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_11.Parent = _112
+UIAspectRatioConstraint_11.AspectRatio = 2.756
+
+UITextSizeConstraint_11.Parent = _112
+UITextSizeConstraint_11.MaxTextSize = 40
+
+_668.Name = "668"
+_668.Parent = Frame1
+_668.BackgroundTransparency = 1.000
+_668.BorderSizePixel = 0
+_668.Position = UDim2.new(0.405940592, 0, 0.333642274, 0)
+_668.Size = UDim2.new(0.0302240755, 0, 0.0426320657, 0)
+_668.Font = Enum.Font.Unknown
+_668.Text = "668"
+_668.TextColor3 = Color3.fromRGB(255, 255, 255)
+_668.TextScaled = true
+_668.TextSize = 40.000
+_668.TextWrapped = true
+_668.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_12.Parent = _668
+UIAspectRatioConstraint_12.AspectRatio = 1.261
+
+UITextSizeConstraint_12.Parent = _668
+UITextSizeConstraint_12.MaxTextSize = 40
+
+_14.Name = "14"
+_14.Parent = Frame1
+_14.BackgroundTransparency = 1.000
+_14.BorderSizePixel = 0
+_14.Position = UDim2.new(0.335591465, 0, 0.388322532, 0)
+_14.Size = UDim2.new(0.0203230847, 0, 0.0444856361, 0)
+_14.Font = Enum.Font.Unknown
+_14.Text = "14"
+_14.TextColor3 = Color3.fromRGB(255, 255, 255)
+_14.TextScaled = true
+_14.TextSize = 40.000
+_14.TextWrapped = true
+_14.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_13.Parent = _14
+UIAspectRatioConstraint_13.AspectRatio = 0.812
+
+UITextSizeConstraint_13.Parent = _14
+UITextSizeConstraint_13.MaxTextSize = 40
+
+_50.Name = "50"
+_50.Parent = Frame1
+_50.BackgroundTransparency = 1.000
+_50.BorderSizePixel = 0
+_50.Position = UDim2.new(0.316310585, 0, 0.277108431, 0)
+_50.Size = UDim2.new(0.0818134472, 0, 0.0843373463, 0)
+_50.Font = Enum.Font.FredokaOne
+_50.Text = "50"
+_50.TextColor3 = Color3.fromRGB(255, 255, 255)
+_50.TextScaled = true
+_50.TextSize = 100.000
+_50.TextWrapped = true
+_50.TextYAlignment = Enum.TextYAlignment.Top
+
+UIAspectRatioConstraint_14.Parent = _50
+UIAspectRatioConstraint_14.AspectRatio = 1.725
+
+UITextSizeConstraint_14.Parent = _50
+
+UIAspectRatioConstraint_15.Parent = Frame1
+UIAspectRatioConstraint_15.AspectRatio = 1.778
+
+UIAspectRatioConstraint_16.Parent = Nighmare
+UIAspectRatioConstraint_16.AspectRatio = 1.778
